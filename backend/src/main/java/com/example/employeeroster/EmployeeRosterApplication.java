6389374b8335d4d0434c1226cf4cf1033b66072d
Manoj M <manumadhu1425@gmail.com>
2024-04-23 11:54:18 +0530

    initial commit

---
+++ backend/src/main/java/com/example/employeeroster/EmployeeRosterApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRosterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/employeeroster/EmployeeRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
