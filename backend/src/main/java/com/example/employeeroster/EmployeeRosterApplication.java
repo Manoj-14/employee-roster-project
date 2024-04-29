ff72902ec28ddb00fe648e7808758c9368837115
Ramakrishna G. Kannari <rkannari@BLR.ALLEGISINDIA.COM>
2024-04-29 11:58:13 +0530

    dto UserResponse

--- backend/src/main/java/com/example/employeeroster/EmployeeRosterApplication.java
+++ backend/src/main/java/com/example/employeeroster/EmployeeRosterApplication.java
@@ -1,13 +1,20 @@
 package com.example.employeeroster;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeRosterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeRosterApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
--- backend/src/main/java/com/example/employeeroster/service/UserService.java
+++ backend/src/main/java/com/example/employeeroster/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.employeeroster.service;
 
+import com.example.employeeroster.dto.UserResponse;
 import com.example.employeeroster.model.User;
 
 import java.util.List;
@@ -10,4 +11,8 @@ public interface UserService {
     public User getUserById(String userId);
     public User updateUser(User user);
     void deleteUser(String userId);
+
+    User findUserByEmailAndPassword(String email, String password);
+
+    public List<UserResponse> getUsers();
 }
